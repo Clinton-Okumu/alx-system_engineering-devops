d5e56e43fbd5686a4e7d3e904dd3e6156c5297af
Clinton-okumu <clintonomondiokumu@gmail.com>
2024-08-13 15:12:28 +0300

    0x16-api_advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,32 @@
 #!/usr/bin/python3
-"""function that queries the Reddit API and returns the
-number of subscribers for a given subreddit"""
+"""
+Function that queries the Reddit API and returns the
+number of subscribers for a given subreddit.
+"""
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """function that queries the Reddit API and returns the number of
-    subscribers for a given subreddit"""
-    url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'My User Agent 1.0'}
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-        return data.get('data').get('subscribers')
-    else:
-        return 0
+    """
+    Queries the Reddit API.
 
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        int: The number of subscribers or 0 if the subreddit is invalid.
+    """
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'My User Agent 1.0'}
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            return data.get('data', {}).get('subscribers', 0)
+        else:
+            return 0
+    except requests.RequestException:
+        # If there's any error (network issue, invalid URL, etc.), return 0
+        return 0
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,24 +1,40 @@
 #!/usr/bin/python3
-"""function that queries the reddit API and prints the
-titles of the first 10 hot posts listed for a given subreddit."""
+"""Function that queries the Reddit API and returns a list of
+titles of all hot articles for a given subreddit.
+"""
 
 import requests
 
 
-def top_ten(subreddit):
-    """function that queries the reddit API and prints the
-    titles of the first 10 hot posts listed for
-    a given subreddit."""
+def recurse(subreddit, hot_list=[], after=None):
+    """Queries the Reddit API recursively to get a list of
+    titles of all hot articles for a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+        hot_list (list): List to store the titles of hot posts.
+        after (str): The "after" parameter for pagination.
+
+    Returns:
+        list: The list containing.
+    """
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            children = response.json().get("data").get("children")
-            for i in range(10):
-                print(children[i].get("data").get("title"))
-        else:
-            print("None")
-    except Exception:
-        print("None")
+    params = {"after": after}
 
+    try:
+        response = requests.get(url, headers=headers,
+                                params=params, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json().get("data")
+            after = data.get("after")
+            children = data.get("children")
+            for child in children:
+                hot_list.append(child.get("data").get("title"))
+            if after is not None:
+                return recurse(subreddit, hot_list, after)
+            return hot_list
+        else:
+            return None
+    except Exception:
+        return None
