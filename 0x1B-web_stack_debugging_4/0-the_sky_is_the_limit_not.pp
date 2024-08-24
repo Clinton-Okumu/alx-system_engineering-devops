8f410a7315c07d8f2615911458e446e34f709c6b
Clinton-okumu <clintonomondiokumu@gmail.com>
2024-08-24 15:58:14 +0300

    Web stack debugging

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,23 +1,14 @@
-# Increase the ULIMIT for Nginx in the default file
+# Increases the ULIMIT for Nginx in the default file
 exec { 'fix--for-nginx':
-  command => 'sed -i "s/^ULIMIT=.*$/ULIMIT=4096/" /etc/default/nginx',
-  path    => ['/bin', '/usr/bin', '/usr/local/bin'],
-  unless  => 'grep -q "^ULIMIT=4096$" /etc/default/nginx',
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/bin:/usr/bin:/usr/local/bin',
   notify  => Exec['nginx-restart'],  # Ensures Nginx restarts after the change
 }
 
-# Restart Nginx to apply the changes
+# Restart Nginx
 exec { 'nginx-restart':
-  command     => 'service nginx restart',
-  path        => ['/bin', '/usr/sbin', '/usr/local/sbin'],
-  refreshonly => true,  # Ensures this runs only if the previous exec is triggered
+  command => 'service nginx restart',  # Correct command to restart Nginx
+  path    => '/bin:/usr/sbin:/usr/local/sbin',
+  refreshonly => true,  # Ensures this runs only if there's a change in the previous exec
 }
 
-# Ensure that Nginx init script sources the default file properly
-file_line { 'ensure-nginx-sources-default':
-  path  => '/etc/init.d/nginx',
-  line  => '. /etc/default/nginx',
-  match => '^. /etc/default/nginx',
-  notify  => Exec['nginx-restart'],  # Restart Nginx if this line is added
-  require => Exec['fix--for-nginx'], # Ensure the ulimit fix runs first
-}
